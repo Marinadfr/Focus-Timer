80d7c7d8bdbc52d85b6a7a77dd7698f179b35351
Marinadfr <marina.fosch@gmail.com>
2023-02-02 09:06:30 -0300

    second big update

--- javascript/pomodoro.js
+++ javascript/pomodoro.js
@@ -1,108 +1,135 @@
-window.onload = () => {
-  /* Pomodoro */
-  let workTime;
-  let breakTime;
-  let timesCompleted; /* Times up*/
-  let cyclesGoal;
-  let cyclesCompleted = 0;
-
-  /* frontend connection */
-  let clock = document.getElementById("clock");
-  let cyclesInput = document.getElementById("cycles-input");
-  let startButton = document.getElementById("start-button");
-  let workTimeInput = document.getElementById("work-time");
-  let breakTimeInput = document.getElementById("break-time");
-  let restTimeInput = document.getElementById("rest-time");
-
-  function populateVariables() {
-    console.log("populated variables");
-    workTime = workTimeInput.value; /* minutes */
-    breakTime = breakTimeInput.value;
-    restTime = restTimeInput.value;
-    cyclesGoal = cyclesInput.value;
-    timesCompleted = 0;
-  }
-
-  /* button functionality */
-  startButton.onclick = () => {
-    populateVariables();
-    startPomodoro();
-  };
-  function startPomodoro() {
-    console.log("Pomodoro started");
-    pomodoroController();
-  }
-
-  function pomodoroController() {
-    if (isRestTime()) {
-      cyclesCompleted++;
-      if (!goalReached()) {
-        currentTime = restTime;
-        timer();
-        timesCompleted = 0;
-      } else {
-        console.log("Pomodoro finished!");
-      }
-      return;
-    }
-    if (timesCompleted % 2 == 0) {
-      /* time to get back to work */
-      currentTime = workTime;
-      timesCompleted++;
-      timer();
-      console.log(
-        "Time to work!:" + timesCompleted + ", cycles:" + cyclesCompleted
-      );
-    } else {
-      /* rest time */
-      currentTime = breakTime;
-      timesCompleted++;
-      timer();
-      console.log(
-        "Time for a break!:" + timesCompleted + ", cycles:" + cyclesCompleted
-      );
-    }
-  }
+const timer = {
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  longBreakInterval: 4,
+  sessions: 0,
 };
 
-/* clock */
-let clockMinutes;
-let clockSeconds;
+let interval;
+
+const buttonSound = new Audio("button-sound.mp3");
+const mainButton = document.getElementById("js-btn");
+mainButton.addEventListener("click", () => {
+  buttonSound.play();
+  const { action } = mainButton.dataset;
+  if (action === "start") {
+    startTimer();
+  } else {
+    stopTimer();
+  }
+});
+
+const modeButtons = document.querySelector("#js-mode-buttons");
+modeButtons.addEventListener("click", handleMode);
+
+function getRemainingTime(endTime) {
+  const currentTime = Date.parse(new Date());
+  const difference = endTime - currentTime;
+
+  const total = Number.parseInt(difference / 1000, 10);
+  const minutes = Number.parseInt((total / 60) % 60, 10);
+  const seconds = Number.parseInt(total % 60, 10);
+
+  return {
+    total,
+    minutes,
+    seconds,
+  };
+}
+
+function startTimer() {
+  let { total } = timer.remainingTime;
+  const endTime = Date.parse(new Date()) + total * 1000;
+
+  if (timer.mode === "pomodoro") timer.sessions++;
+
+  mainButton.dataset.action = "stop";
+  mainButton.textContent = "stop";
+  mainButton.classList.add("active");
+
+  interval = setInterval(function () {
+    timer.remainingTime = getRemainingTime(endTime);
+    updateClock();
+
+    total = timer.remainingTime.total;
+    if (total <= 0) {
+      clearInterval(interval);
+
+      switch (timer.mode) {
+        case "pomodoro":
+          if (timer.sessions % timer.longBreakInterval === 0) {
+            switchMode("longBreak");
+          } else {
+            switchMode("shortBreak");
+          }
+          break;
+        default:
+          switchMode("pomodoro");
+      }
+
+      document.querySelector(`[data-sound="${timer.mode}"]`).play();
+
+      startTimer();
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+
+  mainButton.dataset.action = "start";
+  mainButton.textContent = "start";
+  mainButton.classList.remove("active");
+}
 
 function updateClock() {
-  clockMinutes = formatNumbers(currentTime);
-  clockSeconds = formatNumbers(seconds);
-  clock.innerHTML = clockMinutes + ":" + clockSeconds;
+  const { remainingTime } = timer;
+  const minutes = `${remainingTime.minutes}`.padStart(2, "0");
+  const seconds = `${remainingTime.seconds}`.padStart(2, "0");
+
+  const min = document.getElementById("js-minutes");
+  const sec = document.getElementById("js-seconds");
+  min.textContent = minutes;
+  sec.textContent = seconds;
+
+  const text =
+    timer.mode === "pomodoro" ? "Get back to work!" : "Take a break!";
+  document.title = `${minutes}:${seconds} — ${text}`;
+
+  const progress = document.getElementById("js-progress");
+  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
 }
 
-function formatNumbers(time) {
-  let formattedDigits;
-  if (time < 10) {
-    formattedDigits = "0" + time;
-  } else {
-    formattedDigits = time;
-  }
-  return formattedDigits;
+function switchMode(mode) {
+  timer.mode = mode;
+  timer.remainingTime = {
+    total: timer[mode] * 60,
+    minutes: timer[mode],
+    seconds: 0,
+  };
+
+  document
+    .querySelectorAll("button[data-mode]")
+    .forEach((e) => e.classList.remove("active"));
+  document.querySelector(`[data-mode="${mode}"]`).classList.add("active");
+  document.body.style.backgroundColor = `var(--${mode})`;
+  document
+    .getElementById("js-progress")
+    .setAttribute("max", timer.remainingTime.total);
+
+  updateClock();
 }
 
-/* Timer */
-let currentTime; /* setting minutes */
-let seconds = 0;
+function handleMode(event) {
+  const { mode } = event.target.dataset;
 
-function timer() {
-  if (currentTime > 0 || seconds > 0) {
-    if (seconds == 0) {
-      seconds = 59;
-      currentTime--;
-    } else {
-      seconds--;
-    }
-    updateClock();
-    console.log(currentTime, seconds);
-    setTimeout(timer, 1000);
-  } else {
-    pomodoroController();
-    console.log("Timer has ended!");
-  }
+  if (!mode) return;
+
+  switchMode(mode);
+  stopTimer();
 }
-timer();
+
+document.addEventListener("DOMContentLoaded", () => {
+  switchMode("pomodoro");
+});
